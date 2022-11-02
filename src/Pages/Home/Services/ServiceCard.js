d08695db659a48d34b44b4d450d7fba14634add4
arfan36 <arfanur36@gmail.com>
2022-11-02 09:54:07 +0600

    66-8 (Recap) Create a simple Login page with firebase integration

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ServiceCard = ({ service }) => {
     const { title, img, price } = service;
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-80 bg-base-100 shadow-xl">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,12 +12,12 @@ const Services = () => {
 
     return (
         <div>
-            <div className='text-center mb-4 w-1/2 mx-auto'>
+            <div className='text-center mb-4 w-3/4 mx-auto'>
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Services Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center'>
                 {
                     services.map(service => <ServiceCard
                         key={service._id}
