eafc284b72a70eef2eb02a64c3e76214e96ca0fa
arfan36 <arfanur36@gmail.com>
2022-11-05 08:01:38 +0600

    order added to private route

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -5,6 +5,7 @@ import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Orders from "../../Pages/Orders/Orders";
 import SignUp from "../../Pages/SignUp/SignUp";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 // const { createBrowserRouter } = require("react-router-dom");
 
@@ -32,7 +33,9 @@ const router = createBrowserRouter([
             },
             {
                 path: '/orders',
-                element: <Orders></Orders>
+                element: <PrivateRoute>
+                    <Orders></Orders>
+                </PrivateRoute>
             },
         ]
     },
