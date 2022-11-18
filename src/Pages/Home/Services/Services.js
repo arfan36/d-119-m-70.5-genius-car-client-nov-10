1b2d1b8f60c08ab91880b7d1a40bae82f4a9fd7a
arfan36 <arfanur36@gmail.com>
2022-11-18 16:41:35 +0600

    70_5-2: Genius car find products between price range 100 to 300

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,11 +3,12 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [isAsc, setIsAsc] = useState(true);
     useEffect(() => {
-        fetch('http://localhost:5000/services')
+        fetch(`http://localhost:5000/services?order=${isAsc ? 'asc' : 'dasc'}`)
             .then(res => res.json())
             .then(data => setServices(data));
-    }, []);
+    }, [isAsc]);
 
 
     return (
@@ -16,6 +17,7 @@ const Services = () => {
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Services Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <button className='btn btn-ghost' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'dasc' : 'asc'}</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center'>
                 {
