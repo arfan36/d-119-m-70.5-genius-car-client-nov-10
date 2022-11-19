aeda818bef58ddf4da30802fc2fa89f17a88a656
arfan36 <arfanur36@gmail.com>
2022-11-19 12:10:19 +0600

    70_5-4 Indexing and text search in mongodb

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
     const [isAsc, setIsAsc] = useState(true);
+    const [search, setSearch] = useState('');
+    const searchRef = useRef();
+
     useEffect(() => {
-        fetch(`http://localhost:5000/services?order=${isAsc ? 'asc' : 'dasc'}`)
+        fetch(`http://localhost:5000/services?search=${search}&order=${isAsc ? 'asc' : 'dasc'}`)
             .then(res => res.json())
             .then(data => setServices(data));
-    }, [isAsc]);
+    }, [isAsc, search]);
+
+    // handleSearch
+    const handleSearch = () => {
+        setSearch(searchRef.current.value);
+    };
 
 
     return (
@@ -17,6 +26,7 @@ const Services = () => {
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Services Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <input ref={searchRef} className='input input-bordered input-sm mr-2' type="text" /><button onClick={handleSearch} className='hover:text-red-600'>Search</button>
                 <button className='btn btn-ghost' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'dasc' : 'asc'}</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center'>
